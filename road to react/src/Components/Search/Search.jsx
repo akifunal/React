a4809798e5354587750192937c73c1ac731efad7
akifunal <akifunal05@gmail.com>
2021-09-05 18:16:46 +0300

    search component updated

--- road to react/src/Components/Search/Search.jsx
+++ road to react/src/Components/Search/Search.jsx
@@ -1,10 +1,7 @@
 const Search = ({ onSearch, search }) => (
 	<section className='search'>
-		<label>Search</label>
+		<label htmlFor='search'>Search: </label>
 		<input type='text' id='search' onChange={onSearch} value={search} />
-		<p className='search-header'>
-			Searching for <strong>{search}</strong>
-		</p>
 	</section>
 );
 
